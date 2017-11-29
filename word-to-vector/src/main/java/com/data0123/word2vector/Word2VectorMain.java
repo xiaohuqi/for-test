55d95bb5b09a150d7aa87bb780bb4a8d773b46a7
胡芳槐 <hufh@hiekn.com>
2017-11-29 11:34:04 +0800

    add word2vector module

--- word-to-vector/src/main/java/com/data0123/word2vector/Word2VectorMain.java
+++ word-to-vector/src/main/java/com/data0123/word2vector/Word2VectorMain.java
@@ -1,5 +1,9 @@
 package com.data0123.word2vector;
 
+import com.data0123.word2vector.algorithm.Learn;
+
+import java.io.File;
+
 /**
  * @author xiaohuqi@126.com 2017-10-28 10:20
  **/
@@ -7,9 +11,9 @@ public class Word2VectorMain {
 	public static void main(String[] args) {
 		try{
 			//进行训练
-//			Learn lean = new Learn() ;
-//			lean.learnFile(new File("D:\\work\\nanrui\\kmap\\corpus0.txt")) ;
-//			lean.saveModel(new File("D:\\work\\nanrui\\kmap\\corpus0.model")) ;
+			Learn lean = new Learn() ;
+			lean.learnFile(new File("D:\\work\\nanrui\\kmap\\corpus_all.txt")) ;
+			lean.saveModel(new File("D:\\work\\nanrui\\kmap\\corpus0.model")) ;
 
 			//加载测试
 //			Word2VEC w2v = new Word2VEC() ;
